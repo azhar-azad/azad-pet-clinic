fdc6daaa42610e84353b032f576d5febf470b519
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-06-03 02:01:13 +0600

    Initial Project Setup

---
+++ src/main/java/com/azad/dev/azadpetclinic/AzadPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.azad.dev.azadpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzadPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzadPetClinicApplication.class, args);
+	}
+
+}
